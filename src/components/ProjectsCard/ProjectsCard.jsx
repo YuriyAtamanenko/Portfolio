613c7f383f5021e6d1592f4fca21fb17aaa4c2dc
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2024-02-16 21:05:18 +0200

    Update ProjectsCard.jsx

--- src/components/ProjectsCard/ProjectsCard.jsx
+++ src/components/ProjectsCard/ProjectsCard.jsx
@@ -5,7 +5,11 @@ export default function ProjectsCard({ project }) {
     <Card>
       <img src={project.poster} alt={project.title} height={300} width={350} />
       <h2>{project.title}</h2>
-      <p>{project.languages.map(gener => gener).join(', ')}</p>
+      <p>{project.languages.map(language => language).join(', ')}</p>
+      <p>{project.libraries.map(librarie => librarie).join(', ')}</p>
+      <p>{project.description}</p>
+      <a href={project.link}>View</a>
+      <a href={project.git}>Code</a>
     </Card>
   );
 }
