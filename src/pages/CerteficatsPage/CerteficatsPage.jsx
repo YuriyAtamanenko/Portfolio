79e61eb5628b7c10db2a0a0d6fb6cbcebab9732b
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2024-03-04 09:35:45 +0200

    styled Certificate page

--- src/pages/CerteficatsPage/CerteficatsPage.jsx
+++ src/pages/CerteficatsPage/CerteficatsPage.jsx
@@ -1,13 +1,15 @@
-import { Iframe } from './CerteficatsPage.styled';
+import { Iframe, CertificateWrapper } from './CerteficatsPage.styled';
 
 export default function Certificate() {
   return (
-    <Iframe
-      title="Certificate"
-      src="https://drive.google.com/file/d/15LqkgVrTaIlDTQ4fGch5l0b2ouriVmiF/preview"
-      width="640"
-      height="480"
-      allow="autoplay"
-    ></Iframe>
+    <CertificateWrapper>
+      <Iframe
+        title="Certificate"
+        src="https://drive.google.com/file/d/15LqkgVrTaIlDTQ4fGch5l0b2ouriVmiF/preview"
+        width="640"
+        height="480"
+        allow="autoplay"
+      ></Iframe>
+    </CertificateWrapper>
   );
 }
--- src/pages/CerteficatsPage/CerteficatsPage.styled.js
+++ src/pages/CerteficatsPage/CerteficatsPage.styled.js
@@ -1,7 +1,13 @@
 import styled from '@emotion/styled';
 
+export const CertificateWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 20px 0;
+`;
+
 export const Iframe = styled.iframe`
   width: 800px;
-  height: 1500px;
+  height: 3350px;
   border: none;
 `;
