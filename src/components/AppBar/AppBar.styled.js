b0c493ba74d696484126e892b3898097d921c23b
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2024-03-19 16:56:17 +0200

    Update AppBar.styled.js

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -3,28 +3,30 @@ import { NavLink } from 'react-router-dom';
 
 export const Header = styled.header`
   display: flex;
-  flex-direction: column;
-  gap: 15px;
+  gap: 20px;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
   background-color: #212529;
-  font-size: 20px;
 
   @media screen and (min-width: 1440px) {
     gap: 50px;
-    flex-direction: row;
   }
 `;
 
 export const StyledLink = styled(NavLink)`
   padding: 20px 0px;
-  font-size: 24px;
+  font-size: 20px;
   font-style: normal;
   font-weight: 500;
   line-height: 24px;
 
   color: white;
   text-decoration: none;
+
+  @media screen and (min-width: 1440px) {
+    font-size: 24px;
+  }
 
   &.active {
     color: gold;
